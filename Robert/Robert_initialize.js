d8267e1a219a4730ac2f450e8ad37fde3290785e
sarvasrobert <robertsarvas@gmail.com>
2014-11-23 16:45:22 +0100

    Rewrite initialize, include MyRenderer

--- Robert/Robert_initialize.js
+++ Robert/Robert_initialize.js
@@ -1,3 +1,3 @@
-RenderWidgets.AddRenderer(new BasicThreeRenderer('#basicRendererContainer'));
+RenderWidgets.AddRenderer(new MyRenderer ('#MyRendererContainer'));
 RenderWidgets.AddRenderer(new GraphRenderer('#graphRendererContainer'));
 
