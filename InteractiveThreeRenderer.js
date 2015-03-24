89dd72de75b53176567f846ccb0a1708564d3a3a
szaboova88 <aniko.szaboova28@gmail.com>
2015-03-24 14:31:35 +0100

    Update InteractiveThreeRenderer.js

--- InteractiveThreeRenderer.js
+++ InteractiveThreeRenderer.js
@@ -155,9 +155,9 @@
             if (mesh) {
                 if (newVal) {
                     if (!mesh.defaultMaterial) //if it has no defaultMaterial stored yet, backup the current material
-                    for(i in this.Meshes){
-                        this.Meshes[i] = this.notpickedMaterial;
-                    }
+                    //for (i in this.Meshes) {
+                    //    this.Meshes[i] = this.notpickedMaterial;
+                    //}
                         mesh.defaultMaterial = mesh.material;
                     mesh.material = this.pickedMaterial; //assign it the picked material
                     //TODO STUDENTS this will not work once selection and highlighting are worging, as the materials would easily overwrite each other.
