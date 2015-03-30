4972bb2931087d1b0e5c5c504d21af8c50f7437f
szaboova88 <aniko.szaboova28@gmail.com>
2015-03-30 20:26:11 +0200

    Update InteractiveThreeRenderer.js

--- InteractiveThreeRenderer.js
+++ InteractiveThreeRenderer.js
@@ -63,7 +63,7 @@
     //reference to the mesh being currently picked; null if none
     self.picked = null;
     //material wich substitutes the default mesh material when a mesh is picked
-    var color1 = new THREE.Color(0x#660000);
+    var color1 = new THREE.Color(0x660000);
     self.pickedMaterial = new THREE.MeshBasicMaterial({ color1, blending: THREE.NoBlending });
     self.notpickedMaterial = new THREE.MeshBasicMaterial({ color: 'grey', blending: THREE.NoBlending });
     self.mybasicMaterial = new THREE.MeshNormalMaterial();
