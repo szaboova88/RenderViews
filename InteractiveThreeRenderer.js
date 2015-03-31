7b7f798b9d392e6a596926d8d5b2dd667b27d239
szaboova88 <aniko.szaboova28@gmail.com>
2015-03-31 11:48:52 +0200

    Update InteractiveThreeRenderer.js

--- InteractiveThreeRenderer.js
+++ InteractiveThreeRenderer.js
@@ -66,7 +66,7 @@
     //self.notpickedMaterial = new THREE.MeshNormalMaterial({color: 0x000000});
 
     //self.notpickedMaterial = new THREE.MeshBasicMaterial({ color: 'grey', blending: THREE.NoBlending });
-    self.notpickedMaterial = new THREE.MeshLambertMaterial();
+    self.notpickedMaterial = new THREE.MeshLambertMaterial({ color: 'blue', blending: THREE.NoBlending });
     self.mybasicMaterial = new THREE.MeshNormalMaterial();
 
     //At last we add a new update method
