38798c55ac460a46529c6c892b659f846de25f2e
szaboova88 <aniko.szaboova28@gmail.com>
2016-04-03 21:42:42 +0200

    Update RenderWidgets.js

--- RenderWidgets.js
+++ RenderWidgets.js
@@ -1,7 +1,14 @@
-﻿var RenderWidgets = {
+'use strict'
+var RenderWidgets = {
     Instances: ko.observableArray(),
+    Main3D: null,
 
     AddRenderer: function (renderer) {
+        this.Instances.push(renderer);
+    },
+
+    Add3DRenderer: function (renderer) {
+        this.Main3D = renderer;
         this.Instances.push(renderer);
     },
 
@@ -13,6 +20,55 @@
         });
     },
 
+    UpdateComplete: function () {
+        ko.utils.arrayForEach(this.Instances(), function (item) {
+            if (item.IsInitialized() && item.OnUpdateCompleted)
+                item.OnUpdateCompleted();
+        });
+    },
+
+    UsePrerenderedImage: function (enabled) {
+        ko.utils.arrayForEach(this.Instances(), function (item) {
+            if (item.IsInitialized() && item.hasOwnProperty("imgOverride")) {
+                item.imgOverride = enabled;
+                if (item.container)
+                    $(item.container).css({ opacity: enabled ? 0.001 : 1.0 });
+            }
+        });
+    },
+
+    MainCamera: function () {
+
+        function MitsubaCam(camera) {
+            var pos = camera.position;
+            var up = camera.up;
+
+            var x = new THREE.Vector3();
+            var y = new THREE.Vector3();
+            var z = new THREE.Vector3();
+            camera.matrix.extractBasis(x, y, z);
+            var result = camera.position.clone();
+            result.sub(z);
+
+            return { pos: pos.toArray().toString(), up: up.toArray().toString(), target: result.toArray().toString(), fov: camera.fov };
+        }
+
+        if (this.Main3D) {
+            var container = this.Main3D.container;
+            var cam = MitsubaCam(this.Main3D.camera);
+                
+            return {
+                    width: container.innerWidth(),
+                    height: container.innerHeight(),
+                    pos: cam.pos,
+                    up: cam.up,
+                    target: cam.target,
+                    fov: cam.fov
+                };
+        }
+        else
+            return null;
+    }
     /*
     AllInitialized: function()
     {
