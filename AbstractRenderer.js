97de9a60ad3774e0b17e58de2a3cde3b0618e0b8
szaboova88 <aniko.szaboova28@gmail.com>
2016-04-03 21:40:36 +0200

    Update AbstractRenderer.js

--- AbstractRenderer.js
+++ AbstractRenderer.js
@@ -1,4 +1,5 @@
-﻿//This is an abstract class for a renderer. It connects to the data view model, but it presents nothing.
+'use strict'
+//This is an abstract class for a renderer. It connects to the data view model, but it presents nothing.
 function AbstractRenderer(domQuery) { //for whole window call with domQuery "<body>"
     //Create an empty object. We will return it at the end of this function call.
     var result = {};
@@ -6,17 +7,25 @@ function AbstractRenderer(domQuery) { //for whole window call with domQuery "<bo
     result.containerQuery = domQuery; //Stores the DOM container of the renderer. It should be a tring query for jQuery; e.g. if it is a div with id="abc" then domQuery will be "#abc"
 
     //A multi-value map which stores IDs of shapes as keys and as values objects with subscriptions set as the properties of the objects
-    result.Subscriptions = {};
+    result.ShapeSubscriptions = {};
+    result.SeedSubscriptions = {};
 
     //Arrays of calls. Renderers inheriting from this abstract one should push their methods here. Functions in these arrays should never be called (i.e. executed) directly, but only using the functions defined below.
     result.initCalls = []; //initialization (called only once)
     result.updateCalls = []; //per frame update of interaction, e.g. ray cast query (see animate() function)
     result.renderCalls = []; //per frame drawing (see animate() function)
+    result.resizeCalls = []; //called each time the window is resized
 
     result.addCalls = []; //add new shape to the renderer
     result.removeCalls = []; //remove a shape from the renderer
     result.enableCalls = []; //enable rendering of a seed and all of its shapes
     result.disableCalls = []; //disable rendering of a seed and all of its shapes
+    result.startCalls = []; //when the camera starts to move rendering is activated again
+
+    result.renderingEnabled = true;
+    result.requestStopRendering = true; //no rendering once the camera stops moving
+
+    result.animationFrameId = 0; //in order to be able to cancel the requestAnimationFrame cycle
 
     //Returns true when the renderer has been initialized. lways override this function when inheriting from this renderer.
     result.IsInitialized = function () {
@@ -38,19 +47,57 @@ function AbstractRenderer(domQuery) { //for whole window call with domQuery "<bo
     }
 
     result.render = function () {
+        if (this.renderingEnabled) {
+            for (var i = 0, l = this.renderCalls.length; i < l; i++) {
+                this.renderCalls[i].call(this);
+            }
+
+            if (this.requestStopRendering) {
+                this.requestStopRendering = false;
+                this.renderingEnabled = false;
+            }
+        }
+    }
+
+    result.EnableRendering = function() {
+        for (var i = 0, l = result.startCalls.length; i < l; i++)
+            result.startCalls[i].call(result);
+        result.renderingEnabled = true;
+        result.requestStopRendering = false;
+        result.animate();
+    }
+
+    result.DisableRendering = function() {
+        result.requestStopRendering = true;
+        result.animate();
+    }
+
+    result.RenderSingleFrame = function() {
         for (var i = 0, l = this.renderCalls.length; i < l; i++) {
             this.renderCalls[i].call(this);
         }
     }
 
-    //called once per frame, executes all updates and renders the result
-    result.animate = function () {
+    result.Update = function () {
         for (var i = 0, l = this.updateCalls.length; i < l; i++) {
             this.updateCalls[i].call(this);
         }
+    }
+
+    result.Resize = function () {
+        for (var i = 0, l = this.resizeCalls.length; i < l; i++) {
+            this.resizeCalls[i].call(this);
+        }
+        this.RenderSingleFrame();
+    }
+
+    //called once per frame, executes all updates and renders the result
+    result.animate = function () {
+        this.Update();
         this.render();
 
-        requestAnimationFrame(this.animate.bind(this));
+        if (this.renderingEnabled)
+            requestAnimationFrame(this.animate.bind(this));
     }
 
     result.Init = function () {
@@ -74,14 +121,14 @@ function AbstractRenderer(domQuery) { //for whole window call with domQuery "<bo
         this.render();
     }
 
-    result.addsubscription = function (id, name, callback) {
-        if (result.Subscriptions.hasOwnProperty(id)) {
-            result.Subscriptions[id][name] = callback;
+    result.addShapeSubscription = function (id, name, callback) {
+        if (result.ShapeSubscriptions.hasOwnProperty(id)) {
+            result.ShapeSubscriptions[id][name] = callback;
         }
         else {
             var obj = {};
             obj[name] = callback;
-            result.Subscriptions[id] = obj;
+            result.ShapeSubscriptions[id] = obj;
         }
     }
 
@@ -115,16 +162,16 @@ function AbstractRenderer(domQuery) { //for whole window call with domQuery "<bo
         });
 
         //Both subscriptions to the seed are stored in a singe object and added to the map of subscriptions. Note that there mey be only a single value for each seed.ID, therefore we wrap the two subscriptions to a new object.
-        self.Subscriptions[seed.ID] = { shapes: shapesSubscription, enabled: onOffSubscription };
+        self.SeedSubscriptions[seed.ID] = { shapes: shapesSubscription, enabled: onOffSubscription };
     }
 
     //When a seed is removed, we need to cancel and destroy all subscriptions
     result.RemoveSeedSubscription = function (self, seed) {
         var id = seed.value.ID;
-        if (id in self.Subscriptions) { //If there are any subscriptions stored for this id
-            for (t in self.Subscriptions[id]) //get the subscriptions object for this id
-                self.Subscriptions[id][t].dispose(); //dispose all subscriptions stored in the object
-            delete self.Subscriptions[id]; //delete the whole object from the map of subscriptions
+        if (id in self.SeedSubscriptions) { //If there are any subscriptions stored for this id
+            for (t in self.SeedSubscriptions[id]) //get the subscriptions object for this id
+                self.SeedSubscriptions[id][t].dispose(); //dispose all subscriptions stored in the object
+            delete self.SeedSubscriptions[id]; //delete the whole object from the map of subscriptions
         }        
     }
 
