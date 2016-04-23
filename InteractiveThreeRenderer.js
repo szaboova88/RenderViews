36c427662d1d4d5a91c0699cde2e9af1989ebc7c
szaboova88 <aniko.szaboova28@gmail.com>
2016-04-23 12:00:12 +0200

    Update InteractiveThreeRenderer.js

--- InteractiveThreeRenderer.js
+++ InteractiveThreeRenderer.js
@@ -1,20 +1,20 @@
 'use strict'
-var fsqVertex = 
-'varying vec2 vUv;\
-\
-void main() {\
-    vUv = uv;\
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );\
-}';
+var fsqVertex =
+    'varying vec2 vUv;\
+    \
+    void main() {\
+        vUv = uv;\
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );\
+    }';
 
-var fsqFragment = 
-'varying vec2 vUv;\
-uniform sampler2D tDiffuse;\
-\
-void main() {\
-\
-    gl_FragColor = texture2D( tDiffuse, vUv );\
-}';
+var fsqFragment =
+    'varying vec2 vUv;\
+    uniform sampler2D tDiffuse;\
+    \
+    void main() {\
+    \
+        gl_FragColor = texture2D( tDiffuse, vUv );\
+    }';
 
 function InteractiveThreeRenderer(domQuery) { //for a whole window call with domQuery "<body>"
     //inherit the base class
@@ -26,13 +26,12 @@ function InteractiveThreeRenderer(domQuery) { //for a whole window call with dom
     self.IMeshes = {};
     self.NMeshes = {};
 
-    self.resolveNode = function(mesh)
-    {
+    self.resolveNode = function (mesh) {
         var shapeID = mesh.mName;
         var seedID = self.Seeds[shapeID];
         var _seed = SeedWidgets.GetById(seedID);
         return {shape: _seed.GetShape(shapeID), seed: _seed};
-    }
+    };
 
     //This renderer adds a very basic picking of shapes
     //It sends the results to the shape, which notifies all its subscribers that something has changed.
@@ -41,7 +40,7 @@ function InteractiveThreeRenderer(domQuery) { //for a whole window call with dom
 
     //First we need to add a new initialization call wich will be executed after the one of BasicThreeRenderer
     self.initCalls.push(function () { //push the init function to the list of initCalls
-        this.projector = new THREE.Projector(); //create a new Projector. It will be used to cast a ray through the scene and get the list of intersections of the ray with shapes 
+        this.projector = new THREE.Projector(); //create a new Projector. It will be used to cast a ray through the scene and get the list of intersections of the ray with shapes
         document.addEventListener('mousemove', this.onDocumentMouseMove, false); //each time the mouse moves we want to execute our mouse move routine (defined below)
         document.addEventListener('keydown', this.onDocumentKeyDown, false);
         document.addEventListener('keyup', this.onDocumentKeyUp, false);
@@ -52,9 +51,9 @@ function InteractiveThreeRenderer(domQuery) { //for a whole window call with dom
 
         //Full Screen Quad
         this.fsqScene = new THREE.Scene();
-        
+
         this.fullScreenQuadMaterial = new THREE.ShaderMaterial({
-            uniforms: { tDiffuse: { type: "t", value: this.basicRTT } },
+            uniforms: {tDiffuse: {type: "t", value: this.basicRTT}},
             vertexShader: fsqVertex,
             fragmentShader: fsqFragment,
             depthTest: false,
@@ -73,7 +72,7 @@ function InteractiveThreeRenderer(domQuery) { //for a whole window call with dom
             this.renderer.render(this.fsqScene, this.RTTCamera);
             this.renderer.render(self.debugRays ? this.rayScene : this.interactiveScene, this.camera);
         }
-    }
+    };
 
     self.renderCalls.push(function () {
         self.InteractiveRender();
@@ -84,23 +83,21 @@ function InteractiveThreeRenderer(domQuery) { //for a whole window call with dom
         this.fullScreenQuadMaterial.uniforms.tDiffuse.needsUpdate = true;
     });
 
-    self.mouse = { x: 0, y: 0 }; //here we store the last mouse position. The mouse position is stored only when the mouse moves, but the scene is mostly updated with a much higher frequence
+    self.mouse = {x: 0, y: 0}; //here we store the last mouse position. The mouse position is stored only when the mouse moves, but the scene is mostly updated with a much higher frequence
 
-    self.onDocumentMouseMove = function onDocumentMouseMove( event ) 
-    {
+    self.onDocumentMouseMove = function onDocumentMouseMove(event) {
         //Update the mouse position, a transformation from screen to normalized device coordinates is necessary; notice the flipped y
         self.mouse.x = ((event.clientX - self.container.offset().left) / self.container.innerWidth()) * 2 - 1;
         self.mouse.y = -((event.clientY - self.container.offset().top) / self.container.innerHeight()) * 2 + 1;
         self.Update();
-    }
+    };
 
     self.highlighted = [];
     self.pickingUnlocked = true;
 
     self.debugRays = false;
 
-    self.onDocumentKeyDown = function onDocumentKeyDown(event)
-    {
+    self.onDocumentKeyDown = function onDocumentKeyDown(event) {
         if ((self.picked) && (self.pickingUnlocked) && ((event.key == "Shift") || (event.keyIdentifier == "Shift"))) {
             self.pickingUnlocked = false;
             var node = self.resolveNode(self.picked);
@@ -116,7 +113,7 @@ function InteractiveThreeRenderer(domQuery) { //for a whole window call with dom
         //    self.debugRays = true;
 
         self.Update();
-    }
+    };
 
     self.onDocumentKeyUp = function onDocumentKeyUp(event) {
         while (self.highlighted.length > 0) {
@@ -129,7 +126,7 @@ function InteractiveThreeRenderer(domQuery) { //for a whole window call with dom
         }
         //self.debugRays = false;
         self.Update();
-    }
+    };
 
     //reference to the mesh being currently picked; null if none
     self.picked = null;
@@ -143,9 +140,9 @@ function InteractiveThreeRenderer(domQuery) { //for a whole window call with dom
         polygonOffsetFactor: -1, // positive value pushes polygon further away
         polygonOffsetUnits: 1
     });
-    
+
     //---------------------------------------------
-    
+
     self.notPickedMaterial = new THREE.MeshBasicMaterial({
         color: 'black',
         transparent: true,
@@ -155,10 +152,10 @@ function InteractiveThreeRenderer(domQuery) { //for a whole window call with dom
         polygonOffsetFactor: -1, // positive value pushes polygon further away
         polygonOffsetUnits: 1
     });
-    
+
     //---------------------------------------------
-    
-    self.newGeometry = new THREE.BoxGeometry( 1.2, 1.2, 2 );
+
+    self.newGeometry = new THREE.BoxGeometry(1.2, 1.2, 2);
 
     //At last we add a new update method
     self.updateCalls.push(function () {
@@ -181,35 +178,35 @@ function InteractiveThreeRenderer(domQuery) { //for a whole window call with dom
             ///////// THIS ALTERNATIVE DOES NOT USE THE KNOCKOUT BINDING (and stays here just for educative purposes, please read it first and compare to the approach used below)
             //If any intersection exists
             /*
-            if (intersects.length > 0) {
-                //If the closest mesh intersected is not the currently stored intersection (i.e. picked) mesh
-                if (intersects[0].object != this.picked) {
-                    //Restore previous intersection mash (if anything was picked before) to its original material
-                    if (this.picked) {
-                        this.picked.material = this.picked.defaultMaterial;
-                        this.picked.defaultMaterial = null;
-                    }
-                    //Store reference to closest mesh as current intersection mesh
-                    this.picked = intersects[0].object;
-                    //Store the material of the closest mesh (for later restoration)
-                    this.picked.defaultMaterial = this.picked.material;
-                    //Set a new material for closest mesh
-                    this.picked.material = this.pickedMaterial;                
-                }
-            }
-            else //There are no intersections
-            {
-                //Restore previous intersection mesh (if it exists) to its original material
-                if (this.picked) {
-                    this.picked.material = this.picked.defaultMaterial;
-                    this.picked.defaultMaterial = null;
-                }
-                //Remove previous intersection mesh reference by setting current intersection object to null
-                this.picked = null;
-            }
-            */
+             if (intersects.length > 0) {
+             //If the closest mesh intersected is not the currently stored intersection (i.e. picked) mesh
+             if (intersects[0].object != this.picked) {
+             //Restore previous intersection mash (if anything was picked before) to its original material
+             if (this.picked) {
+             this.picked.material = this.picked.defaultMaterial;
+             this.picked.defaultMaterial = null;
+             }
+             //Store reference to closest mesh as current intersection mesh
+             this.picked = intersects[0].object;
+             //Store the material of the closest mesh (for later restoration)
+             this.picked.defaultMaterial = this.picked.material;
+             //Set a new material for closest mesh
+             this.picked.material = this.pickedMaterial;
+             }
+             }
+             else //There are no intersections
+             {
+             //Restore previous intersection mesh (if it exists) to its original material
+             if (this.picked) {
+             this.picked.material = this.picked.defaultMaterial;
+             this.picked.defaultMaterial = null;
+             }
+             //Remove previous intersection mesh reference by setting current intersection object to null
+             this.picked = null;
+             }
+             */
 
-            ///////// THIS ALTERNATIVE USES THE KNOCKOUT BINDING        
+            ///////// THIS ALTERNATIVE USES THE KNOCKOUT BINDING
             if (intersects.length > 0) {
                 //If the closest mesh intersected is not the currently stored intersection (i.e. picked) mesh
                 if (intersects[0].object != this.picked) {
@@ -241,98 +238,102 @@ function InteractiveThreeRenderer(domQuery) { //for a whole window call with dom
     });
 
     //////// AS KNOCKOUT IS USED, WE NEED TO SUBSCRIBE TO THE PICKED STATE CHANGES
-    self.addCalls.push(function(shape)
-    {
+    self.addCalls.push(function (shape) {
 
-        var id = shape.id;
-        console.log(self);
-        //if (!self.Meshes.hasOwnProperty(id))
-        var m = self.Meshes[id].clone();
-        var n = self.Meshes[id].clone();
+            var id = shape.id;
+            console.log(self);
+            //if (!self.Meshes.hasOwnProperty(id))
+            var m = self.Meshes[id].clone();
+            var n = self.Meshes[id].clone();
             console.log(m);
             console.log(n);
             m.material = self.pickedMaterial;
             n.material = self.notPickedMaterial;
 
-        m.mName = id;
-        n.mName = id;
+            m.mName = id;
+            n.mName = id;
 
-        //console.log('self.newGeometry');
-        //console.log(self.newGeometry);
-        //m.geometry = self.newGeometry;
-        m.scale.set(1.5,1.5,1.5);
-        m.matrixAutoUpdate = true;
+            //console.log('self.newGeometry');
+            //console.log(self.newGeometry);
+            //m.geometry = self.newGeometry;
+            m.scale.set(1.5, 1.5, 1.5);
+            m.matrixAutoUpdate = true;
 
-        console.log(m);
-        console.log(n);
-        console.log(id);
-        console.log('self.IMeshes');
-        console.log(self.IMeshes);
-        console.log('self.NMeshes');
-        console.log(self.NMeshes);
+            console.log(m);
+            console.log(n);
+            console.log(id);
+            console.log('self.IMeshes');
+            console.log(self.IMeshes);
+            console.log('self.NMeshes');
+            console.log(self.NMeshes);
 
-        self.IMeshes[id] = m;
-        self.NMeshes[id] = n;
-        
-        if (shape.interaction.visible()) {
-            self.rayScene.add(m);
-            self.rayScene.add(n);
+            self.IMeshes[id] = m;
+            self.NMeshes[id] = n;
+
+            if (shape.interaction.visible()) {
+                self.rayScene.add(m);
+                self.rayScene.add(n);
             }
 
-        var pickSubscription = shape.interaction.picked.subscribe(function (newVal, bbbb) {
-            var mesh = this.IMeshes[id]; //get the mesh for the shape
-            console.log(bbbb);
-            if (mesh) {
-                if (newVal) {
-                    //this.interactiveScene.add(this.NMeshes[6]);
-                    for (var item in this.NMeshes) {
-                       if (item != id) { 
-                            if(this.NMeshes[item].parent) {
-                                this.interactiveScene.add(this.NMeshes[item]);
-                            };
-                        };
-                    }
-                    this.interactiveScene.add(mesh);
-             
-                }
-                else {
-                    if (shape.interaction.visible())
-                        {
+            var pickSubscription = shape.interaction.picked.subscribe(function (newVal, bbbb) {
+                var mesh = this.IMeshes[id]; //get the mesh for the shape
+                console.log(bbbb);
+                if (mesh) {
+                    if (newVal) {
+                        //this.interactiveScene.add(this.NMeshes[6]);
+                        for (var item in this.NMeshes) {
+                            if (item != id) {
+                                if (this.NMeshes[item].parent) {
+                                    this.interactiveScene.add(this.NMeshes[item]);
+                                }
+                            }
+                        }
+                        this.interactiveScene.add(mesh);
+                    } else {
+                        if (shape.interaction.visible()) {
                             //this.rayScene.add(this.NMeshes[6]);
                             this.rayScene.add(mesh);
                             for (var item in this.NMeshes) {
-                                if (item != id) { 
-                                    if(this.NMeshes[item].parent) {
+                                if (item != id) {
+                                    if (this.NMeshes[item].parent) {
                                         this.rayScene.add(this.NMeshes[item]);
-                                    };
-                                };
+                                    }
+                                }
                             }
-                        }
-                    else {
+                        } else {
                             this.interactiveScene.remove(mesh);
                             //this.interactiveScene.remove(this.NMeshes[6]);
-                                               for (var item in this.NMeshes) {
-                       if (item != id) { 
-                            if(this.NMeshes[item].parent) {
-                                this.interactiveScene.remove(this.NMeshes[item]);
+                            for (var item in this.NMeshes) {
+                                if (item != id) {
+                                    if (this.NMeshes[item].parent) {
+                                        this.interactiveScene.remove(this.NMeshes[item]);
+                                    }
+                                }
                             }
-                            };
                         }
-                        }
+                    }
+                    //this.RenderSingleFrame();
+                    self.InteractiveRender();
                 }
-                //this.RenderSingleFrame();
+            }.bind(self));
+
+            var selectSubscription = shape.interaction.selected.subscribe(function (newVal) {
+                console.log('selectSubscription');
+
                 self.InteractiveRender();
+            }.bind(self));
+
+            //Add the subscription to the subscriptions map for the given shape, note that again we use an object to possibly store several subscriptions per shape
+            if (id in this.ShapeSubscriptions) {
+                this.ShapeSubscriptions[id].pick = pickSubscription;
+                this.ShapeSubscriptions[id].select = selectSubscription;
+            } else {
+                this.ShapeSubscriptions[id] = {pick: pickSubscription};
+                this.ShapeSubscriptions[id] = {select: selectSubscription};
             }
-        }.bind(self));
 
-        //Add the subscription to the subscriptions map for the given shape, note that again we use an object to possibly store several subscriptions per shape
-        if (id in this.ShapeSubscriptions)
-            this.ShapeSubscriptions[id].pick = pickSubscription;
-        else
-            this.ShapeSubscriptions[id] = { pick: pickSubscription };
-
-        //TODO STUDENTS add subscriptions to other shape properties in a similar way
-    }
+            //TODO STUDENTS add subscriptions to other shape properties in a similar way
+        }
     );
 
     //The seed subscriptions stored in this.subscriptions are disposed by the AbstractRenderer, so we don't need to take care of them here. No push to removeCalls necessary.
