e32197cbfccf2d1788bf28c20b6df0197e297330
szaboova88 <aniko.szaboova28@gmail.com>
2016-04-26 21:16:35 +0200

    Update Aniko_initialize.js

--- Aniko/Aniko_initialize.js
+++ Aniko/Aniko_initialize.js
@@ -1,1 +1,9 @@
 RenderWidgets.Add3DRenderer(new InteractiveThreeRenderer('#basicRendererContainer'));
+
+  alert('tusom');
+    $('#clone-button').html('kokotina');
+    $('#clone-button').prop('value', 'Save');    
+    
+    $('#parse-button').click(function() {
+        alert( "Handler for .click() called." );
+    });
