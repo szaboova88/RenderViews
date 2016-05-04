eb17ba0366b96dc7ecaaaf6ef2f4c82867d41ab2
szaboova88 <aniko.szaboova28@gmail.com>
2016-05-04 20:36:47 +0200

    Update Aniko_initialize.js

--- Aniko/Aniko_initialize.js
+++ Aniko/Aniko_initialize.js
@@ -1,3 +1,3 @@
 RenderWidgets.Add3DRenderer(new InteractiveThreeRenderer('#basicRendererContainer'));
 
-$('#grammar-group').append('<div class="motion_box"><input type="checkbox" id="blur_effect" name="blureffects" value="Blur"><label for="blur_effect">Blur</label></div>');
+$('#grammar-group').append('<div class="motion_box" style="position: fixed;right: 0px;top: 50px;width: 150px;height: 100px;background-color: #f5f5f5;border: 1px solid #ccc;"><input type="checkbox" id="blur_effect" name="blureffects" value="Blur"><label for="blur_effect">Depth of field</label></div>');
