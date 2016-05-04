c91924ae2e0a6e14e7677989983020b36cdbb8c3
szaboova88 <aniko.szaboova28@gmail.com>
2016-05-04 20:28:05 +0200

    Update Aniko_initialize.js

--- Aniko/Aniko_initialize.js
+++ Aniko/Aniko_initialize.js
@@ -1,3 +1,3 @@
 RenderWidgets.Add3DRenderer(new InteractiveThreeRenderer('#basicRendererContainer'));
 
-$('#grammar-group').append('<div><input type="checkbox" id="blur_effect" name="blureffects" value="Blur"><label for="blur_effect">Blur</label></div>');
+$('#grammar-group').append('<div class="motion_box"><input type="checkbox" id="blur_effect" name="blureffects" value="Blur"><label for="blur_effect">Blur</label></div>');
