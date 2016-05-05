0b3e26ccbb26034391984939612c584b57562b01
szaboova88 <aniko.szaboova28@gmail.com>
2016-05-05 23:13:35 +0200

    Update VerticalTiltShiftShader.js

--- VerticalTiltShiftShader.js
+++ VerticalTiltShiftShader.js
@@ -15,7 +15,7 @@ THREE.VerticalTiltShiftShader = {
 
 		"tDiffuse": { type: "t", value: null },
 		"v":        { type: "f", value: 1.0 / 512.0 },
-		"r":        { type: "f", value: 0.35 }
+		"r":        { type: "f", value: 0.10 }
 
 	},
 
